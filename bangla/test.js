ed23528293e127166993b8c1c5b59c039e126f80
fmferhat6446 <mhmdforhad333@gmail.com>
2024-06-30 05:20:56 +0000

    js practice

--- bangla/test.js
+++ bangla/test.js
@@ -1,27 +1,55 @@
 // console.log("ferhat");
-console.log("hitesh")
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+// console.log("hitesh")
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
 
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
+// buttons.forEach(function (button) {
+//   console.log(button);
+//   button.addEventListener('click', function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === 'grey') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'white') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'blue') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'yellow') {
+//       body.style.backgroundColor = e.target.id;
+//     }
     
-  });
-});
+//   });
+// });
+
+console.log("ferhat");
+
+const bigNumber = 4758675885899948n
+
+// console.log(typeof bigNumber);
+
+const hero = function () {
+  console.log("hello world");
+}
+
+// hero()
+
+let myName = "fmferhat"
+
+let nkName = myName
+nkName = "Apon"
+
+// console.log(myName);
+
+
+// console.log(`hello my name is ${myName} and my repo count`);
+
+let myArr = ["'batman", "superman", "spiderman", "antman", "fatman"]
+
+// console.log(myArr[2]);
+
+
 
 
